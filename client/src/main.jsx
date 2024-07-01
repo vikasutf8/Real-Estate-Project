28f7a8542a29840d031ce44adfe544bf9ef117ae
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-07-02 02:01:45 +0530

    working on deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,9 @@ import './index.css';
 import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+// import {disableReactDevTooles} from '@fvilers/disable-react-devtools'
+
+// if(process.env.)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
